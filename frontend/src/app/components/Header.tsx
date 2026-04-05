a3a4a553bcf7f44c5bebcbdb76963f2f797c38bb
Hannah Bang <hannahbang@outlook.com>
2026-04-05 10:29:36 -0400

    linking

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -1,20 +1,25 @@
 export default function Header() {
-   return (
-     <header className="w-full bg-green-primary px-10 h-[44px] flex items-center justify-start gap-12">
-       <span className="text-[#F7F4EC] font-sans text-[20px] font-bold">
-         LOGO
-       </span>
-       <nav className="flex gap-8">
-         {["Who we are", "What we do", "Our Teams", "Contact"].map((link) => (
-           <a
-             key={link}
-             href="#" // update w links
-             className="text-beige-primary font-nimbus text-sm hover:font-bold transition-all"
-             >
-             {link}
-           </a>
-         ))}
-       </nav>
-     </header>
-   );
- }
+  return (
+    <header className="w-full bg-green-primary px-10 h-[44px] flex items-center justify-start gap-12">
+      <span className="text-[#F7F4EC] font-sans text-[20px] font-bold">
+        LOGO
+      </span>
+      <nav className="flex gap-8">
+        {[
+          { label: "Who we are", href: "/about" },
+          { label: "What we do", href: "" },
+          { label: "Our Teams", href: "" },
+          { label: "Contact", href: "/contact" },
+        ].map((link) => (
+          <a
+            key={link.label}
+            href={link.href}
+            className="text-beige-primary font-nimbus text-sm hover:font-bold transition-all"
+          >
+            {link.label}
+          </a>
+        ))}
+      </nav>
+    </header>
+  );
+}
