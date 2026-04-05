c77f78bf7d9199c9a9ef0946ae7a43491570ca63
Hannah Bang <hannahbang@outlook.com>
2026-04-05 10:59:39 -0400

    top

---
+++ frontend/src/app/our_teams/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function OurTeams() {
+    return (
+        <div className="min-h-screen w-full">
+            <div className="bg-background max-w-[1000px] mx-auto">
+                <h1 className="text-left pt-10"><b>Welcome to NUMAC</b></h1>
+                <img src="https://placehold.co/800x400" alt="placeholder" className="w-full mt-4" />
+            </div>
+        </div>
+    );
+}
