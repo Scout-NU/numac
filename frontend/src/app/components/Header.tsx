420055be8538da5dc8c1568367815c634e17327c
Hannah Bang <65321838+hannahbang1108@users.noreply.github.com>
2026-04-14 22:21:17 -0400

    [FE] Teams Page (#16)
    * top
    
    * final changes
    
    * small changes
    
    * sizing
    
    * lint

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -3,13 +3,18 @@ export default function Header() {
   return (
     <header className="w-full bg-green-primary px-10 h-[44px] flex items-center justify-start gap-12">
       <span className="text-[#F7F4EC] font-sans text-[20px] font-bold">
-        LOGO
+          <a
+            key={"LOGO"}
+            href={"/"}
+          >
+            LOGO
+          </a>
       </span>
       <nav className="flex gap-8">
         {[
           { label: "Who we are", href: "/about" },
-          { label: "What we do", href: "" },
-          { label: "Our Teams", href: "" },
+          { label: "What we do", href: "/projects" },
+          { label: "Our Teams", href: "/our_teams" },
           { label: "Contact", href: "/contact" },
         ].map((link) => (
           <a
@@ -23,4 +28,4 @@ export default function Header() {
       </nav>
     </header>
   );
-}
+ }
